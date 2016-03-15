13c10583154c4c4d5758f8b8515656a9b2326911
moyoyoya <ulises.moya@gmail.com>
2016-03-14 21:31:55 -0600

    repo clonado de bliss

--- redSocial/urls.py
+++ redSocial/urls.py
@@ -1,21 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from cuentas import views
-from posts import views as views2
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', 
+    	admin.site.urls),
 
-    url(r'^login/$',
-    	views.LoginView.as_view(),
-    	name='login'
-    	),
+    url(r'^cuentas/', 
+    	include('cuentas.urls')),
 
-    url(r'^todos/$',
-    	views2.PostView.as_view(),
-    	name="todos"),
-
-    
-
+    url(r'^posts/', 
+    	include('posts.urls'))
     
 ]
